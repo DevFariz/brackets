073c473f7ac910a3e9b203340be92b55a4a081bd
R. Fariz <farizramazanov38@gmail.com>
2022-02-17 13:32:53 +0400

    docs: partly solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let elements = [];
+    let openBrackets = ['[', '(', '{'];
+    let bracketPairs = {
+        [']'] : '[',
+        [')'] : '(',
+        ['}'] : '{',
+    }
+
+    let count = 0;
+
+    for(let i = 0; i < str.length; i++){
+        let element = str[i];
+
+        if(element === '|'){
+            count++;
+        }else{
+            if(openBrackets.includes(element)){
+                elements.push(element)
+            }else{
+                if(elements.length === 0){
+                    return false;
+                }
+    
+                let topElement = elements[elements.length - 1];
+    
+                if(bracketPairs[element] === topElement){
+                    elements.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        
+    }
+
+    if(count % 2 != 0){
+        return false;
+    }
+
+    return elements.length === 0;
+    
 }
